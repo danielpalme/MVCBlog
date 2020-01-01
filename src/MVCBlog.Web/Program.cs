a61f3882ebb5c4c0d9db9477c408f952b5fc13f4
danielpalme <daniel.palme@gmx.de>
2020-01-01 21:18:12 +0100

    Improved logging

--- src/MVCBlog.Web/Program.cs
+++ src/MVCBlog.Web/Program.cs
@@ -3,10 +3,8 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
-
 
 namespace MVCBlog.Web
 {
@@ -46,13 +44,7 @@ namespace MVCBlog.Web
                           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
                     config.AddEnvironmentVariables();
                 })
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logging.AddConsole();
-                    logging.AddDebug();
-                    logging.AddSerilog(dispose: true);
-                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
@@ -66,6 +58,7 @@ namespace MVCBlog.Web
                .Enrich.FromLogContext()
                .WriteTo.RollingFile("Logs/Log-{Date}.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}")
                .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level >= Serilog.Events.LogEventLevel.Error).WriteTo.RollingFile("Logs/Errors/ErrorLog-{Date}.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}"))
+               .WriteTo.Console()
                .CreateLogger();
     }
 }
